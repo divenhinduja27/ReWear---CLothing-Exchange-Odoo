5515a27712e5777e36630b3c8861bab3aacd6d5f
divenhinduja27 <divenhinduja27@gmail.com>
2025-07-12 14:57:21 +0530

    created navbar,landing page and coverpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './home/landing/landing.component';
+import { CoverPageComponent } from './home/cover-page/cover-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CoverPageComponent
   ],
   imports: [
     BrowserModule,
