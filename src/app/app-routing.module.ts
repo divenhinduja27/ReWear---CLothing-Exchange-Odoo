f2d422701c1bf7824a7f3ce008527a880568193b
divenhinduja27 <divenhinduja27@gmail.com>
2025-07-12 12:03:01 +0530

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoverPageComponent } from './home/cover-page/cover-page.component';
+import { LandingComponent } from './home/landing/landing.component';
+import { ItemListingComponent } from './products/item-listing/item-listing.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AdminPanelComponent } from './home/admin-panel/admin-panel.component';
+import { UserDashboardComponent } from './auth/user-dashboard/user-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:CoverPageComponent
+  },
+  {
+    path:'landing',
+    component:LandingComponent
+  },
+  {
+    path:'itemListing',
+    component:ItemListingComponent
+  },
+  {
+    path:'productDetail',
+    component:ProductDetailComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'adminPanel',
+    component:AdminPanelComponent
+  },
+  {
+    path:'userDashboard',
+    component:UserDashboardComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
